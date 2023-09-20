f9daba8700266fd6d82325e3eb031a0e2d1947a1
Lord Machiavelli <tmakaveli643@gmail.com>
2023-09-20 23:35:00 +0100

    header

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -1,5 +1,11 @@
+"use client"
+
 import * as React from "react"
 import bck from "./../../public/assets/bck.jpg"
+import Link from "next/link"
+//GiTriangleTarget import that from react-icon
+import { GiTriangleTarget } from "react-icons/gi"
+import { PiHamburgerLight } from "react-icons/pi"
 const Hero = ({ ...props }) => {
   return (
     <main className="w-full h-auto">
@@ -9,7 +15,31 @@ const Hero = ({ ...props }) => {
           backgroundImage: `url('${bck.src}')`,
         }}
       >
-        <div className="w-full h-full bg-[#0000005d]  bg-opacity-50 backdrop-filter backdrop-blur-3xl"></div>
+        <div className="w-full h-full bg-[#0000005d]  bg-opacity-50 backdrop-filter backdrop-blur-3xl">
+          <header className="w-full h-auto flex items-center justify-between p-7">
+            <Link href={"/"} title="Alaa Barka">
+              <GiTriangleTarget
+                //change color
+                color="#fff"
+                //size
+                size={40}
+              />
+            </Link>
+
+            <button
+              className="w-12 h-12 rounded-full bg-[#ffffff1d] flex items-center justify-center border border-white"
+              type="button"
+              onClick={() => {}}
+            >
+              <PiHamburgerLight
+                //change color
+                color="#fff"
+                //size
+                size={30}
+              />
+            </button>
+          </header>
+        </div>
       </div>
     </main>
   )
