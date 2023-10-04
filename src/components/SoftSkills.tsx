6b01d43f439edbb6866df14882de87fd9351e091
Lord Machiavelli <tmakaveli643@gmail.com>
2023-10-04 05:53:02 +0100

    mpp

--- src/components/SoftSkills.tsx
+++ src/components/SoftSkills.tsx
@@ -1,21 +1,8 @@
 import * as React from "react"
+import Communication from "./../../public/lottie/communication.json"
+import Lottie from "lottie-react"
 
 const SoftCard = ({ ...props }) => {
-  interface softSkills {
-    title: string
-    animation: any
-    description: string
-    colors: string[]
-  }
-  const mySoftSkills: softSkills[] = [
-    {
-      title: "Communication Skills",
-      animation: "",
-      description:
-        "Clear and persuasive communication is the cornerstone of effective interactions.",
-      colors: ["#FFCC70", "#C850C0"],
-    },
-  ]
   return (
     <>
       <div
@@ -29,12 +16,92 @@ const SoftCard = ({ ...props }) => {
   )
 }
 const SoftSkills = ({ ...props }) => {
+  interface softSkills {
+    title: string
+    animation: any
+    description: string
+    colors: string[]
+  }
+  const mySoftSkills: softSkills[] = [
+    {
+      title: "Communication Skills",
+      animation: (
+        <Lottie
+          animationData={Communication}
+          ref={React.useRef(null)}
+          loop
+          autoplay
+          className=""
+        />
+      ),
+      description:
+        "Clear and persuasive communication is the cornerstone of effective interactions.",
+      colors: ["#FFCC70", "#C850C0"],
+    },
+    {
+      title: "Communication Skills",
+      animation: (
+        <Lottie
+          animationData={Communication}
+          ref={React.useRef(null)}
+          loop
+          autoplay
+          className=""
+        />
+      ),
+      description:
+        "Clear and persuasive communication is the cornerstone of effective interactions.",
+      colors: ["#FFCC70", "#C850C0"],
+    },
+    {
+      title: "Communication Skills",
+      animation: (
+        <Lottie
+          animationData={Communication}
+          ref={React.useRef(null)}
+          loop
+          autoplay
+          className=""
+        />
+      ),
+      description:
+        "Clear and persuasive communication is the cornerstone of effective interactions.",
+      colors: ["#FFCC70", "#C850C0"],
+    },
+    {
+      title: "Communication Skills",
+      animation: (
+        <Lottie
+          animationData={Communication}
+          ref={React.useRef(null)}
+          loop
+          autoplay
+          className=""
+        />
+      ),
+      description:
+        "Clear and persuasive communication is the cornerstone of effective interactions.",
+      colors: ["#FFCC70", "#C850C0"],
+    },
+  ]
   return (
     <>
       <div className="w-full h-auto lg:px-36">
         <p className="text-white text-xl ">Learn some of my Soft-Skills</p>
 
-        <div className="w-full h-auto flex items-center justify-center gap-10 py-10"></div>
+        <div className="w-full h-auto flex items-center justify-center gap-10 py-10">
+          {mySoftSkills.map((skill: softSkills, index: number) => {
+            return (
+              <>
+                <SoftCard
+                  From={skill.colors[0]}
+                  To={skill.colors[1]}
+                  key={index}
+                />
+              </>
+            )
+          })}
+        </div>
       </div>
     </>
   )
