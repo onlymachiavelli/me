66d4896ee0839a51e6786803f9764ca7816fa134
Lord Machiavelli <tmakaveli643@gmail.com>
2023-10-04 01:23:52 +0100

    responsive description

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -5,8 +5,8 @@ const Description = () => {
   const astroRef = React.useRef(null)
   return (
     <>
-      <div className="w-full h-auto lg:h-screen flex items-center justify-center">
-        <aside className="w-1/2 h-full flex items-center justify-center">
+      <div className="w-full h-auto lg:h-screen flex items-center justify-center py-10 lg:py-0 flex-col lg:flex-row">
+        <aside className="w-full lg:w-1/2 h-full flex items-center justify-center">
           <Lottie
             className="w-2/3"
             animationData={astro}
@@ -15,8 +15,8 @@ const Description = () => {
             ref={astroRef}
           />
         </aside>
-        <aside className="w-1/2 h-full grid content-center  ">
-          <p className="text-white text-sm w-4/5 ">
+        <aside className="w-full lg:w-1/2 h-full grid content-center  ">
+          <p className="text-white text-sm w-4/5 m-auto lg:m-0 ">
             Hello there! My name is Alaa Barka, and I am a Full Stack Web
             Developer and Cross Platform Developer.
             <br />
@@ -28,7 +28,7 @@ const Description = () => {
             skills to create innovative solutions.
           </p>
 
-          <button className="w-48 border block m-left mt-10 border-white text-white px-6 py-3 hover:bg-white hover:text-gray-900 hover:border-transparent transition duration-300 ease-in-out text-sm font-thin">
+          <button className="w-48 m-auto lg:m-0 border block m-left mt-10 border-white text-white px-6 py-3 hover:bg-white hover:text-gray-900 hover:border-transparent transition duration-300 ease-in-out text-sm font-thin">
             Say My name
           </button>
         </aside>
