670441b2fab5738ba4b9fe027b8e441c5b20ef05
Lord Machiavelli <tmakaveli643@gmail.com>
2023-10-06 00:11:54 +0100

    half responsive slider

--- src/components/hardskill.tsx
+++ src/components/hardskill.tsx
@@ -4,16 +4,33 @@ import "swiper/css"
 import "swiper/css/pagination"
 import "swiper/css/navigation"
 import { Pagination, Navigation, Keyboard, Mousewheel } from "swiper/modules"
-
+import { Devops } from "./icons"
 const HardSwiper = ({ ...props }) => {
   return (
     <>
       <div
-        className="w-full h-[40rem]  rounded"
+        className="w-full py-5 lg:py-0 h-[45rem]  rounded flex items-center justify-center lg:flex-row flex-col-reverse"
         style={{
           backgroundImage: `linear-gradient(45deg,#00DBDE, #FC00FF)` as string,
         }}
-      ></div>
+      >
+        <aside className="w-full lg:w-2/3 flex items-center p-10 flex-col">
+          <h1 className="text-white text-4xl font-bold ">
+            I do DevOps & cloud engineering
+          </h1>
+
+          <p className="text-white pl-4 py-5">
+            In my role as a DevOps and cloud engineering professional, I
+            recognize that clear and persuasive communication is pivotal for
+            fostering effective collaboration and achieving our objectives
+            within the intricate domains of software development, system
+            operations, and cloud infrastructure management.
+          </p>
+        </aside>
+        <aside className="w-full lg:w-1/3 flex items-center justify-center">
+          <Devops Width={300} />
+        </aside>
+      </div>
     </>
   )
 }
@@ -27,6 +44,8 @@ const HardSkills: React.FC = ({ ...props }) => {
 
         <Swiper
           spaceBetween={1}
+          //space
+          slidesPerView={1}
           pagination={{
             clickable: true,
           }}
@@ -42,14 +61,23 @@ const HardSkills: React.FC = ({ ...props }) => {
             invert: false,
           }}
           keyboard
-          className="mySwiper !w-full !py-10 !px-10 !gap-5"
+          className="mySwiper !w-full !py-10  "
         >
-          <SwiperSlide className=" !w-full !h-auto">
+          <SwiperSlide className=" !w-full !h-auto !mx-3 ">
             <HardSwiper />
           </SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide className=" !w-full !h-auto !mx-3">
+            <HardSwiper />
+          </SwiperSlide>
+          <SwiperSlide className=" !w-full !h-auto !mx-3">
+            <HardSwiper />
+          </SwiperSlide>
+          <SwiperSlide className=" !w-full !h-auto  !mx-3">
+            <HardSwiper />
+          </SwiperSlide>
+          <SwiperSlide className=" !w-full !h-auto !mx-3">
+            <HardSwiper />
+          </SwiperSlide>
         </Swiper>
       </section>
     </>
